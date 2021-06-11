363038891dac34293624578749469883035acc2a
Danylo Torianikov <danylo.torianicov@gmail.com>
2021-06-11 18:45:04 +0300

    add angular material

---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
+import { HeaderComponent } from './header/header.component';
+import { TaskComponent, DialogOverviewExampleDialog } from './task/task.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {HttpClientModule} from '@angular/common/http';
+
+import { ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {DemoMaterialModule} from './material-module';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
+// import {TaskComponent, DialogOverviewExampleDialog} from './task/task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TaskComponent,
+    DialogOverviewExampleDialog
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([AppEffects]),
+    BrowserAnimationsModule,
+    MatSliderModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+
+  entryComponents: [TaskComponent, DialogOverviewExampleDialog],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ],
+  bootstrap: [AppComponent, TaskComponent]
+})
+export class AppModule { }
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,104 @@
+
+import { Component, OnInit, Inject  } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { addTask, clear, deleteTask, editTask, taskSelector, executionSelector } from '../reducers/task';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {FormGroup, FormControl} from '@angular/forms'
+
+export interface DialogData {
+  text: string;
+  vibor: string;
+  selectRankExport: string;
+}
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+
+export class TaskComponent implements OnInit {
+
+  // execuionForm = new FormGroup({
+  //   a: new FormControl(''),
+  //   b: new FormControl(''),
+  //   c: new FormControl('')
+  // });
+
+  text: string;
+  vibor: string;
+  selectRankExport: string;
+  buk: string
+  
+  textNewTask: string;
+
+  arrTask$ = this.store.select(taskSelector);
+  execuion$ = this.store.select(executionSelector);
+
+  execuionClient: string;
+
+  constructor(private store: Store, public dialog: MatDialog ) { }
+
+  
+  openDialog(): void {
+    
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '550px',
+      data: {text: this.text, vibor:this.vibor, selectRankExport: this.selectRankExport }
+    
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      
+      this.store.dispatch(addTask({textNewTask: result.text, executionClient: result.selectRankExport }));
+    
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  clear(){
+
+    this.store.dispatch(clear());
+    this.textNewTask = '';
+
+  }
+
+  deleteTask(index){
+    
+    this.store.dispatch(deleteTask({index: index}));
+   
+  }
+
+  edit(index, item, buk){
+    
+    // let upadateExecution;
+
+    // if(b1) upadateExecution = 'not_performed';
+    // if(b2) upadateExecution = 'doing';
+    // if(b3) upadateExecution = 'done';
+    console.log(buk);
+
+    this.store.dispatch(editTask({index:index, upadateTask: item, upadateExecution: buk  }))
+  }
+
+  
+
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
